d67ffc31558442dab916e3c56e69558a443c850b
r2magarcia <ma.garcia2r@gmail.com>
2020-11-02 18:34:07 -0500

    scroll chat

--- src/app/pages/private/home/components/chat-area/chat-area.component.ts
+++ src/app/pages/private/home/components/chat-area/chat-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { ChatService } from 'src/app/shared/services/chat/chat.service';
 import { MessageI } from '../../interfaces/MessageI';
 
@@ -7,18 +7,28 @@ import { MessageI } from '../../interfaces/MessageI';
   templateUrl: './chat-area.component.html',
   styleUrls: ['./chat-area.component.scss']
 })
-export class ChatAreaComponent implements OnInit {
+export class ChatAreaComponent implements OnInit, AfterViewChecked {
 
   @Input() title: string = ""
   @Input() icon: string = ""
   @Input() msgs: Array<MessageI> = []
+  @ViewChild('chat') private myScrollContainer: ElementRef;
 
   msg: string;
 
   constructor(public chatService: ChatService) { }
 
+  element:HTMLElement;
+  scrolled:boolean=false;
+  
+
   ngOnInit(): void {
+    this.element= document.getElementById("chat") as HTMLElement;
+    this.scrollToBottom();
+    this.element.addEventListener("scroll",(e)=>this.scrolled=true);
   }
+
+
 
   sendMsg() {
     const msg: MessageI = {
@@ -30,5 +40,20 @@ export class ChatAreaComponent implements OnInit {
     }
     this.chatService.sendMsg(msg);
     this.msg = "";
+
   }
+  ngAfterViewChecked() {      
+    if(!this.scrolled){
+      this.scrollToBottom();   
+    }  
+    // console.log("que tanto scroll tiene? "+this.myScrollContainer.nativeElement.scrollTop);   
+  } 
+
+  scrollToBottom(): void {
+    try {
+        this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch(err) { }   
+    // console.log("que tanto scroll tiene? "+this.myScrollContainer.nativeElement.scrollHeight);               
+    }
+  
 }
